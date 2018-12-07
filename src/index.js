34f6cf3faf4ab2c619b17ba01919694c963d78e0
andy8052 <andychorlian@gmail.com>
2018-12-06 22:00:25 -0500

    adding props for fees and relayer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 
+const relays = {
+  "radar": "https://api.radarrelay.com/0x/v2/",
+  "bamboo": "https://sra.bamboorelay.com/0x/v2/"
+}
+
 class Instant extends React.Component {
 
   componentDidMount() {
+    let source = this.props.source != undefined && relays[this.props.source] != undefined ? relays[this.props.source] : relays["radar"]
+
+    let renderJson;
+    if (this.props.feeAddress != undefined && this.props.fee != undefined && parseFloat(this.props.fee) <= 0.05 && parseFloat(this.props.fee) > 0) {
+      renderJson = {
+          orderSource: source,
+          affiliateInfo: {
+            feeRecipient: this.props.feeAddress,
+            feePercentage: parseFloat(this.props.fee)
+          }
+      }
+    } else {
+      renderJson = {
+          orderSource: source
+      }
+    }
+
     const script = document.createElement("script");
     script.src = "https://instant.0xproject.com/instant.js";
     document.head.appendChild(script);
 
     window.onload = function() {
-      console.log("got here")
-      window.zeroExInstant.render({
-          orderSource: 'https://api.radarrelay.com/0x/v2/',
-      }, 'body');
+      window.zeroExInstant.render(renderJson, 'body');
     };
   }
 
